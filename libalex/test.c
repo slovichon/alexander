58e5fb3077bbe69f9407f4ca4bf2141edc5b5c02
jaredy <>
2003-09-01 05:30:22 +0000

    added void *** casts

--- libalex/test.c
+++ libalex/test.c
@@ -11,20 +11,20 @@ int main(int argc, char *argv[])
 	extern char *_prefix;
 	_prefix = "";
 
-	vlist_init(&a, 4, sizeof(int *));
+	vlist_init((void ***)&a, 4, sizeof(int *));
 
 	for (i = 0; i < 6; i++) {
 		if ((b = (int *)malloc(sizeof(int))) == NULL)
 			err(2, "unable to malloc()");
 		*b = i;
 bark("[test] adding %d", *b);
-		vlist_add(&a, b);
+		vlist_add((void ***)&a, b);
 	}
 
-	while ((b = vlist_remove(&a)) != NULL)
+	while ((b = vlist_remove((void ***)&a)) != NULL)
 		printf("Retrieved %d\n", *b);
 
-	vlist_free(&a);
+	vlist_free((void ***)&a);
 
 	printf("done!\n");
 
